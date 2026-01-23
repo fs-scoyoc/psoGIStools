99318112d342cdbdc8888ce79ebf7dde55453284
Matthew W. Van Scoyoc <matthew.vanscoyoc@usda.gov>
2026-01-23 10:40:55 -0700

    updated get_landfire() to include any product

--- R/landfire_functions.R
+++ R/landfire_functions.R
@@ -1,10 +1,11 @@
 #' Download LANDFIRE Existing Vegetation Type (250EVT)
 #' 
-#' This function downloads and saves LANDFIRE Existing Vegetation Type (250EVT) 
-#'     raster data using the `rlandfire` package and summarizes the data by EVT 
-#'     Name and EVT Physiognomy for the area of interest.
+#' This function downloads and saves LANDFIRE raster data using the `rlandfire` 
+#'     package and the raster data masked to the AoI polygon and the attribute 
+#'     table as a list.
 #'
 #' @param aoi_polygon Area of interest `sf` polygon object.
+#' @param lf_products Vector of LANDFIRE products. See (Products Table)<https://lfps.usgs.gov/products>. Defult is `250EVT`.
 #' @param lf_dir Directory path to save raster to.
 #' @param email_address Email address. Passesd on to [rlandfire::landfireAPIv2()].
 #' @param res Raster resolution. Default is 30.
@@ -23,7 +24,8 @@
 #' # Get LANDFIRE EVT data
 #' lf_plan_area <- get_landfire(plan_area, file.path("data", "landfire"), 
 #'                              "your.name@usda.gov")
-get_landfire_evt <- function(aoi_polygon, lf_dir, email_address, res = 30){
+get_landfire <- function(aoi_polygon, lf_products = "250EVT", lf_dir, 
+                             email_address, res = 30){
   # aoi_polygon = targets::tar_read(plan_area)
   # lf_dir = file.path("data", "LANDFIRE")
   # email_address = Sys.getenv("GBIF_EMAIL")
@@ -36,7 +38,7 @@ get_landfire_evt <- function(aoi_polygon, lf_dir, email_address, res = 30){
   # Generate AoA wkt string
   lf_aoi = rlandfire::getAOI(aoa_sf)
   # Pull EVT data from LANDFIRE API
-  resp = rlandfire::landfireAPIv2(products = "250EVT", aoi = lf_aoi,
+  resp = rlandfire::landfireAPIv2(products = lf_products, aoi = lf_aoi,
                                   email_address, resolution = res,
                                   path = tempfile(fileext = ".zip"),
                                   method = 'auto', verbose = FALSE)
@@ -65,25 +67,8 @@ get_landfire_evt <- function(aoi_polygon, lf_dir, email_address, res = 30){
   ) |> 
     janitor::clean_names() |> 
     dplyr::select(-count, -lfrdb)
-  # Summarize masked raster and join attribut table
-  evt_data = terra::extract(lf_aoa, aoa_sf) |> 
-    janitor::clean_names() |> 
-    dplyr::group_by(evt_name) |> 
-    dplyr::summarise(count = dplyr::n()) |> 
-    dplyr::left_join(attr_table, by = "evt_name") |> 
-    dplyr::mutate(area_m2 = count * prod(terra::res(lf_aoa)), 
-                  acres = area_m2 / 4046.86, 
-                  pct_area = (area_m2 / sum(plan_area_proj$area_m2) * 100), 
-                  .groups = 'drop')
-  phys_data = evt_data |> 
-    dplyr::group_by(evt_phys, evt_gp_n, evt_sbcls, evt_name) |> 
-    dplyr::summarise(count = sum(count), 
-                     area_m2 = sum(area_m2), 
-                     acres = sum(acres), 
-                     pct_area = (area_m2 / sum(plan_area_proj$area_m2) * 100), 
-                     .groups = 'drop')
   # Return summarized data
-  return(list("EVT" = evt_data, "PHYS" = phys_data))
+  return(list("lf_raster" = lf_aoa, "attribute_table" = attr_table))
 }
 
 
