18af2aab109b7c1c2ae338a35e06dbc1c8420a60
Matthew W. Van Scoyoc <matthew.vanscoyoc@usda.gov>
2026-02-25 11:24:21 -0700

    updated read_edw_lyr()

--- R/read_edw_lyr.R
+++ R/read_edw_lyr.R
@@ -1,33 +1,43 @@
 #' Read spatial data from Forest Service ArcGIS REST Services
-#'
-#' read_edw_lyr reads features from the Forest Service ArcGIS REST Services
-#'     Directory, https://apps.fs.usda.gov/arcx/rest/services/EDW, using the
-#'     `arcgislayers` package.
-#'
+#' 
+#' This function reads spatial features from the Forest Service ArcGIS REST 
+#'     Services. Either the public ArcGIS REST Service, 
+#'     https://apps.fs.usda.gov/arcx/rest/services/EDW, or the internal ArcGIS 
+#'     REST Service, https://apps.fs.usda.gov/arcn/rest/services/EDW, using the 
+#'     `arcgislayers` package. You must be on a Forest Service network to access 
+#'     data from the interal ArcGIS REST Service.
 #'
 #' @param map_name Character. Name of map layer.
 #' @param layer Integer. Number of layer to read. Default is  zero (0).
-#' @param crs Coordinate reference system (crs). Default is EPSG:4326 (WGS 84).
+#' @param service Character. The public ("arcx") or internal ("arcn") ArcGIS 
+#'     REST Service code. Default is "arcx". You must be on a Forest Service 
+#'     network for "arcn" to work.
+#' @param target_crs Coordinate reference system (crs). Default is EPSG:4326 
+#'      (WGS 84).
 #'
 #' @return An [sf] object or [terra::SpatRaster-class].
 #' @seealso [arcgislayers::arc_read()], [sf::st_transform()]
 #' @export
-#'
+#' 
 #' @examples
-#' library("GIStools")
-#'
+#' library(mpsgSE)
+#' 
 #' # Administrative Boundary for the Dixie National Forest
-#' admin_bndry <- read_edw_lyr("EDW_ForestSystemBoundaries_01", layer = 1) |>
+#' admin_bndry <- read_edw_lyr("EDW_ForestSystemBoundaries_01", layer = 1) |> 
 #'   dplyr::filter(forestname == "Dixie National Forest")
-read_edw_lyr <- function(map_name, layer = 0, crs = "EPSG:4326"){
-  # map_name = "EDW_ForestSystemBoundaries_01"
-  # layer = 1
-  edw_rest <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/"
+#' 
+read_edw_lyr <- function(map_name, layer = 0, service = "arcx", 
+                         crs = "EPSG:4326"){
+  
+  # map_name = "EDW_BioTESP_01"; layer = 1; service = "arcn"
+  # map_name = "EDW_ForestSystemBoundaries_01"; layer = 0; service = "arcx"
+  
+  edw_rest <- glue::glue("https://apps.fs.usda.gov/{service}/rest/services/EDW/")
   lyr = arcgislayers::arc_read(
-    glue::glue(edw_rest, "{map_name}/MapServer/{layer}")
+    glue::glue("{edw_rest}/{map_name}/MapServer/{layer}")
   ) |>
-    janitor::clean_names() |>
-    sf::st_make_valid() |>
-    sf::st_transform(crs)
+    janitor::clean_names() |> 
+    sf::st_make_valid() |> 
+    sf::st_transform(crs = crs)
   return(lyr)
 }
